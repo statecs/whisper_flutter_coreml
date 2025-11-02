3a6290deef8be8de0697efb10d28d6e1f77fefc1
statecs <statec@icloud.com>
2025-11-02 20:52:48 +0100

    large-v3-turbo-q8_0

--- lib/download_model.dart
+++ lib/download_model.dart
@@ -23,7 +23,7 @@ enum WhisperModel {
   /// medium model for all languages
   medium("medium-q8_0", 1269, 1269),
   /// turbo model for all languages
-  turbo("large-v3-turbo", 3000, 3557),
+  turbo("large-v3-turbo-q8_0", 1536, 1536),
   /// large model for all languages
   largeV1("large-v1", 3000, 3557),
   largeV2("large-v2", 3000, 3557),
